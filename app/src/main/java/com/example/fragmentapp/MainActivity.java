15516672913a4793f10ea2491478437f03579d49
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-05-11 10:19:09 +0700

    second push

--- app/src/main/java/com/example/fragmentapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentapp/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.fragmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnfragment1, btnfragment2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnfragment1 = findViewById(R.id.btnfragment1);
+        btnfragment2 = findViewById(R.id.btnfragment2);
+        btnfragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadfragment(new PertamaFragment();
+            }
+        });
+
+        btnfragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadfragment(new KeduaFragment();
+            }
+        });
+
+
+
     }
+
+    private void loadfragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.framelayout,fragment);
+        transaction.commit();
+    }
+
 }
